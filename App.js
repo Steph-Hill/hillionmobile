a021d0d3f2a2d5ce5ee686927e0f285e977830ce
Stephen Hillion <100539972+Steph-Hill@users.noreply.github.com>
2022-07-08 09:47:23 -0400

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import AppIntroSlider from 'react-native-app-intro-slider';
 const slides = [
 
   {
-    key:"one",
+    key:"one1",
     title:"JUST TRAVEL",
     text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
     image:require("./images/1.jpg")
